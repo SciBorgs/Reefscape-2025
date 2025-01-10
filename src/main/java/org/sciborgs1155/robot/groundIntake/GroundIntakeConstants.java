0133002ede283c2888ee33610073315be2a87e49
Unclesdad <basaltjazz3@gmail.com>
2025-01-10 16:48:37 -0500

    worked on main java file but also made changes to IO interface

---
+++ src/main/java/org/sciborgs1155/robot/groundIntake/GroundIntakeConstants.java
@@ -0,0 +1,18 @@
+package org.sciborgs1155.robot.groundIntake;
+
+import static edu.wpi.first.units.Units.Radians;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public class GroundIntakeConstants {
+    
+    //TODO not tuned (at all)
+    public static final double kP = 1;
+    public static final double kI = 0;
+    public static final double kD = 0.01;
+
+    public static final Rotation2d INTAKE_ANGLE = Rotation2d.fromRadians(0); //TODO figure this out
+    public static final Rotation2d OUTTAKE_ANGLE = Rotation2d.fromRadians(0); //TODO figure this one out too
+    public static final Rotation2d STARTING_ANGLE = Rotation2d.fromRadians(0); //TODO ??????
+    
+}
