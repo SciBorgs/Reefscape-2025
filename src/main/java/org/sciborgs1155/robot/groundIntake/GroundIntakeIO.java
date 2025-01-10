2dd508795b1c15dd3216ed37813ceb2b01daa02e
Unclesdad <basaltjazz3@gmail.com>
2025-01-10 16:31:19 -0500

    made IO interface

---
+++ src/main/java/org/sciborgs1155/robot/groundIntake/GroundIntakeIO.java
@@ -0,0 +1,19 @@
+package org.sciborgs1155.robot.groundIntake;
+
+public interface GroundIntakeIO {
+
+    /**
+     * @return The position in radians.
+     */
+    public double position();
+
+    /**
+     * @return The position in radians/sec.
+     */
+    public double velocity();
+
+    /**
+     * Sets the voltage of the arm motor.
+     */
+    public void set();
+}
