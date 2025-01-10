ad0303ed9485958332e371ef8bbf2bc58edf6e9b
HenryLi-0 <bxsci8266@gmail.com>
2025-01-10 17:38:26 -0500

    configs for realscoral

--- src/main/java/org/sciborgs1155/robot/scoral/RealScoral.java
+++ src/main/java/org/sciborgs1155/robot/scoral/RealScoral.java
@@ -1,9 +1,16 @@
 package org.sciborgs1155.robot.scoral;
 
+import static edu.wpi.first.units.Units.Amps;
+import static org.sciborgs1155.lib.FaultLogger.check;
 import static org.sciborgs1155.robot.Ports.Scoral.*;
+import static org.sciborgs1155.robot.scoral.ScoralConstants.CURRENT_LIMI;
 
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 public class RealScoral implements ScoralIO {
@@ -18,10 +25,39 @@ public class RealScoral implements ScoralIO {
     bottomMotor = new SparkMax(BOTTOM_ROLLER, MotorType.kBrushed);
 
     beambreak = new DigitalInput(BEAMBREAK);
+
+    SparkMaxConfig topConfig = new SparkMaxConfig();
+    check(
+        topMotor,
+        topMotor.configure(
+            topConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters));
+
+    topConfig.apply(
+        topConfig.idleMode(IdleMode.kBrake).smartCurrentLimit((int) CURRENT_LIMI.in(Amps)));
+
+    check(
+        topMotor,
+        topMotor.configure(
+            topConfig, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters));
+
+    SparkMaxConfig bottomConfig = new SparkMaxConfig();
+    check(
+        bottomMotor,
+        bottomMotor.configure(
+            bottomConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters));
+
+    bottomConfig.apply(bottomConfig.apply(topConfig).follow(TOP_ROLLER, true));
+
+    check(
+        bottomMotor,
+        bottomMotor.configure(
+            bottomConfig, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters));
   }
 
   @Override
-  public void setPower(double power) {}
+  public void setPower(double power) {
+    topMotor.set(power);
+  }
 
   @Override
   public boolean beambreak() {
--- src/main/java/org/sciborgs1155/robot/scoral/ScoralConstants.java
+++ src/main/java/org/sciborgs1155/robot/scoral/ScoralConstants.java
@@ -1,5 +1,10 @@
 package org.sciborgs1155.robot.scoral;
 
+import static edu.wpi.first.units.Units.Amps;
+
+import edu.wpi.first.units.measure.Current;
+
 public class ScoralConstants {
   public static final double POWER = 0.5;
+  public static final Current CURRENT_LIMI = Amps.of(45);
 }
