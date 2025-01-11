b0551d7b98ef87c3f0f75643d577dae27c4d2736
ethan jones <jonese>
2025-01-11 14:26:38 -0500

    spotless my beloved

--- src/main/java/org/sciborgs1155/robot/elevator/RealElevator.java
+++ src/main/java/org/sciborgs1155/robot/elevator/RealElevator.java
@@ -3,18 +3,16 @@ package org.sciborgs1155.robot.elevator;
 import static org.sciborgs1155.robot.Ports.Elevator.*;
 import static org.sciborgs1155.robot.elevator.ElevatorConstants.*;
 
-import org.sciborgs1155.lib.TalonUtils;
-
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import org.sciborgs1155.lib.TalonUtils;
 
 public class RealElevator implements ElevatorIO {
   // idk how many there are
   private final TalonFX leader = new TalonFX(LEADER);
   private final TalonFX follower = new TalonFX(FOLLOWER);
-  
 
   public RealElevator() {
     TalonFXConfiguration drumConfig = new TalonFXConfiguration();
@@ -22,7 +20,7 @@ public class RealElevator implements ElevatorIO {
 
     drumConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
     drumConfig.Feedback.SensorToMechanismRatio = CONVERSION_FACTOR;
-  
+
     leader.getConfigurator().apply(drumConfig);
     TalonUtils.addMotor(leader);
   }
