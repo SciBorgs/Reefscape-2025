56d6682638c13e2a5249e8da5c4fafe66c8172d2
Sunay Jain <sunay1j@gmail.com>
2025-01-21 10:12:44 -0500

    removed arm and coroller subsystem

--- src/main/java/org/sciborgs1155/robot/arm/Arm.java
+++
@@ -1,9 +0,0 @@
-package org.sciborgs1155.robot.arm;
-
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-public class Arm extends SubsystemBase {
-  public static Arm create() {
-    return new Arm();
-  }
-}
--- src/main/java/org/sciborgs1155/robot/coroller/Coroller.java
+++
@@ -1,9 +0,0 @@
-package org.sciborgs1155.robot.coroller;
-
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-public class Coroller extends SubsystemBase {
-  public static Coroller create() {
-    return new Coroller();
-  }
-}
