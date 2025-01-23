a89e7e63c97113c0153086f19731820be94d9af1
ethan jones <jonese>
2025-01-23 14:16:50 -0500

    spotless

--- src/main/java/org/sciborgs1155/robot/scoral/Scoral.java
+++ src/main/java/org/sciborgs1155/robot/scoral/Scoral.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-
 import java.util.Optional;
 import monologue.Annotations.Log;
 import monologue.Logged;
