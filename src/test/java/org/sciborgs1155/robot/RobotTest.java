07a2ccbce271d828dc7dc8be4de2145a6c627c9a
sigalrmp <sigalrmp@gmail.com>
2025-01-24 15:56:42 -0500

    more experimenting...

--- src/test/java/org/sciborgs1155/robot/RobotTest.java
+++ src/test/java/org/sciborgs1155/robot/RobotTest.java
@@ -1,15 +1,10 @@
 package org.sciborgs1155.robot;
 
-import static org.sciborgs1155.lib.UnitTestingUtil.reset;
-import static org.sciborgs1155.lib.UnitTestingUtil.setupTests;
-
 import org.junit.jupiter.api.Test;
 
 public class RobotTest {
   @Test
   void setup() throws Exception {
-    setupTests();
-    new Robot().close();
-    reset();
+    new Robot();
   }
 }
