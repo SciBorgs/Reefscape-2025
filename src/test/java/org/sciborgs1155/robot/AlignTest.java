7e03f417c948aabceff07e3d0a0108e776b15314
Unclesdad <basaltjazz3@gmail.com>
2025-02-01 19:22:18 -0500

    spotless

--- src/test/java/org/sciborgs1155/robot/AlignTest.java
+++ src/test/java/org/sciborgs1155/robot/AlignTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.sciborgs1155.lib.UnitTestingUtil.reset;
 import static org.sciborgs1155.lib.UnitTestingUtil.runToCompletion;
 import static org.sciborgs1155.lib.UnitTestingUtil.setupTests;
+import static org.sciborgs1155.robot.Constants.Field.Branch.*;
 import static org.sciborgs1155.robot.Constants.allianceReflect;
 
 import com.pathplanner.lib.pathfinding.LocalADStar;
@@ -14,10 +15,15 @@ import edu.wpi.first.hal.AllianceStationID;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.simulation.DriverStationSim;
-import edu.wpi.first.wpilibj2.command.Command;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.sciborgs1155.robot.Constants.Field;
 import org.sciborgs1155.robot.Constants.Field.Branch;
 import org.sciborgs1155.robot.commands.Alignment;
@@ -30,13 +36,34 @@ import org.sciborgs1155.robot.scoral.Scoral;
 
 public class AlignTest {
 
+  Drive drive;
+  Elevator elevator;
+  Scoral scoral;
+  Alignment align;
+
   @BeforeEach
   public void setup() {
     setupTests();
+    drive = Drive.create();
+    elevator = Elevator.create();
+    scoral = Scoral.create();
+    drive.resetEncoders();
+
+    // Configure pathfinding libraries
+    LocalADStar pathfinder = new LocalADStar();
+    Pathfinding.setPathfinder(pathfinder);
+    Autos.configureAutos(drive);
+
+    align = new Alignment(drive, elevator, scoral);
+  }
+
+  @AfterEach
+  public void destroy() throws Exception {
+    reset();
   }
 
   @RepeatedTest(5)
-  public void reflectionTest() throws Exception {
+  public void reflectionTest() {
     Random rand = new Random();
     DriverStationSim.setAllianceStationId(AllianceStationID.Red1);
     DriverStationSim.notifyNewData();
@@ -53,39 +80,49 @@ public class AlignTest {
         bluePose.getRotation().rotateBy(Rotation2d.fromRotations(0.5)), redPose.getRotation());
     DriverStationSim.resetData();
     DriverStationSim.notifyNewData();
-    reset();
   }
 
-  // Sometimes it fails due to PathPlanner issues.
-  // This never occurs in practice, however, so we can ignore those "rare" occurrences.
+  /**
+   * Tests whether the non-obstacle-avoiding pathing works correctly.
+   *
+   * <p>Sometimes it fails due to PathPlanner issues, so there is a failure threshold. This never
+   * occurs in practice, however, so we can ignore those "rare" occurrences.
+   */
   @RepeatedTest(value = 15, failureThreshold = 5)
   public void pathfindTest() throws Exception {
-    // Make subsystems
-    Drive drive = Drive.create();
-    Elevator elevator = Elevator.create();
-    Scoral scoral = Scoral.create();
-    drive.resetEncoders();
-
     // Take a random branch pose
     Random rand = new Random();
     Pose2d pose = Branch.values()[rand.nextInt(Branch.values().length)].pose;
 
-    // Configure pathfinding libraries
-    LocalADStar pathfinder = new LocalADStar();
-    Pathfinding.setPathfinder(pathfinder);
-    Autos.configureAutos(drive);
+    // Make and run the pathfinding command
+    runToCompletion(align.pathfind(pose));
 
-    // Create command class and make a command
-    Alignment align = new Alignment(drive, elevator, scoral);
-    Command testcmd = align.pathfind(pose).withTimeout(20);
-
-    // Test that the command works
-    runToCompletion(testcmd);
+    // Assert the command works
     assertEquals(pose.getX(), drive.pose().getX(), Translation.TOLERANCE.in(Meters));
     assertEquals(pose.getY(), drive.pose().getY(), Translation.TOLERANCE.in(Meters));
     assertEquals(
         0,
         pose.getRotation().minus(drive.pose().getRotation()).getRadians(),
         Rotation.TOLERANCE.in(Radian));
+  }
+
+  /** Tests whether the non-obstacle-avoiding pathing works correctly. */
+  @ParameterizedTest()
+  @MethodSource("pathGoals")
+  public void pathingTest(Branch branch) {
+    // Create and run directPathFollow
+    runToCompletion(align.directPathfollow(branch.pose));
+
+    // Assert that the command works
+    assertEquals(branch.pose.getX(), drive.pose().getX(), Translation.TOLERANCE.in(Meters));
+    assertEquals(branch.pose.getY(), drive.pose().getY(), Translation.TOLERANCE.in(Meters));
+    assertEquals(
+        0,
+        branch.pose.getRotation().minus(drive.pose().getRotation()).getRadians(),
+        Rotation.TOLERANCE.in(Radian));
+  }
+
+  private static Stream<Arguments> pathGoals() {
+    return Arrays.stream(Branch.values()).map(branch -> Arguments.of(branch));
   }
 }
