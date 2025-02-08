969fc88ab85bb71b5582ca7bc7a410bbc36a2cae
HenryLi-0 <bxsci8266@gmail.com>
2025-02-08 11:42:45 -0500

    alternatingColor for leds and fix led length

--- src/main/java/org/sciborgs1155/robot/led/LEDStrip.java
+++ src/main/java/org/sciborgs1155/robot/led/LEDStrip.java
@@ -58,13 +58,11 @@ public class LEDStrip extends SubsystemBase implements Logged, AutoCloseable {
                             + 0.5)));
   }
 
-  /** A solid yellow green that is scrolled through. */
+  /** An alernating pattern of yellow and green that is scrolled through. */
   public Command scrolling() {
     return set(
-        LEDPattern.solid(Color.kGreenYellow)
-            .mask(
-                LEDPattern.steps(Map.of(0, Color.kWhite, 0.5, Color.kBlack))
-                    .scrollAtAbsoluteSpeed(MetersPerSecond.of(0.5), LED_SPACING)));
+      alternatingColor(Color.kYellow, 6, Color.kGreen, 10)
+                    .scrollAtAbsoluteSpeed(MetersPerSecond.of(0.5), LED_SPACING));
   }
 
   /** A breathing gradient that matches the alliance colors. */
@@ -87,6 +85,16 @@ public class LEDStrip extends SubsystemBase implements Logged, AutoCloseable {
           pattern.applyTo(buffer);
           led.setData(buffer);
         });
+  }
+
+  private static LEDPattern alternatingColor(
+      Color color1, int color1length, Color color2, int color2length) {
+    return (reader, writer) -> {
+      int bufLen = reader.getLength();
+      for (int i = 0; i < bufLen; i++) {
+        writer.setLED(i, (((i % (color1length + color2length)) < color1length) ? color1 : color2));
+      }
+    };
   }
 
   @Override
