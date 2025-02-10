a831b32384e7f3386e8bfa93a170a4882363b351
HenryLi-0 <bxsci8266@gmail.com>
2025-02-10 17:15:47 -0500

    forgot to add spotless

--- src/main/java/org/sciborgs1155/robot/drive/TalonOdometryThread.java
+++ src/main/java/org/sciborgs1155/robot/drive/TalonOdometryThread.java
@@ -1,24 +1,23 @@
 package org.sciborgs1155.robot.drive;
 
-public class TalonOdometryThread extends Thread{
+public class TalonOdometryThread extends Thread {
 
-    private static TalonOdometryThread instance = null;
+  private static TalonOdometryThread instance = null;
 
-    public static TalonOdometryThread getInstance() {
-        if (instance == null) {
-            instance = new TalonOdometryThread();
-        }
-        return instance;
+  public static TalonOdometryThread getInstance() {
+    if (instance == null) {
+      instance = new TalonOdometryThread();
     }
+    return instance;
+  }
 
-    @Override
-    public synchronized void start() {
-        super.start();
-    }
+  @Override
+  public synchronized void start() {
+    super.start();
+  }
 
-    @Override
-    public void run() {
-        // TODO write
-    }
-    
+  @Override
+  public void run() {
+    // TODO write
+  }
 }
