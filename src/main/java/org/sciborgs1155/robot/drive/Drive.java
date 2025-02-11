8806d45cfb6a8fb8df7b4d064d0e462b8fd6787b
HenryLi-0 <bxsci8266@gmail.com>
2025-02-11 15:39:43 -0500

    fix sim crash

--- src/main/java/org/sciborgs1155/robot/drive/Drive.java
+++ src/main/java/org/sciborgs1155/robot/drive/Drive.java
@@ -165,6 +165,7 @@ public class Drive extends SubsystemBase implements Logged, AutoCloseable {
 
     modules = List.of(this.frontLeft, this.frontRight, this.rearLeft, this.rearRight);
     modules2d = new FieldObject2d[modules.size()];
+    lastPositions = modulePositions();
 
     translationCharacterization =
         new SysIdRoutine(
