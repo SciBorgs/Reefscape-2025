fe0c01eabadba299047cc596a73efd564c6aee67
HenryLi-0 <bxsci8266@gmail.com>
2025-02-15 13:06:45 -0500

    ledsegment.java

--- src/main/java/org/sciborgs1155/robot/led/LEDConstants.java
+++ src/main/java/org/sciborgs1155/robot/led/LEDConstants.java
@@ -9,4 +9,8 @@ public class LEDConstants {
   public static final int LED_LENGTH = 120;
   // The distance between two LEDs on the LED Strip.
   public static final Distance LED_SPACING = Meters.of(0.01);
+
+  public static final LEDSegment LEFT_LED_SEGMENT = new LEDSegment(0, 59);
+  public static final LEDSegment RIGHT_LED_SEGMENT = new LEDSegment(60, 119);
+  public static final LEDSegment[] LED_SEGMENTS = {LEFT_LED_SEGMENT, RIGHT_LED_SEGMENT};
 }
--- src/main/java/org/sciborgs1155/robot/led/LEDStrip.java
+++ src/main/java/org/sciborgs1155/robot/led/LEDStrip.java
@@ -1,21 +1,14 @@
 package org.sciborgs1155.robot.led;
 
-import static edu.wpi.first.units.Units.MetersPerSecond;
-import static edu.wpi.first.units.Units.Seconds;
 import static org.sciborgs1155.robot.Ports.LEDs.*;
 import static org.sciborgs1155.robot.led.LEDConstants.*;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.LEDPattern;
-import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import java.util.function.DoubleSupplier;
 import monologue.Logged;
-import org.sciborgs1155.robot.Constants;
 
 public class LEDStrip extends SubsystemBase implements Logged, AutoCloseable {
 
@@ -29,71 +22,12 @@ public class LEDStrip extends SubsystemBase implements Logged, AutoCloseable {
     led.start();
   }
 
-  /** Rainbow LEDs, scrolling at 0.5 m/s. Very cool. */
-  public Command rainbow() {
-    return set(
-        LEDPattern.rainbow(225, 225).scrollAtAbsoluteSpeed(MetersPerSecond.of(0.5), LED_SPACING));
-  }
-
-  /**
-   * A gradient of green to yellow LEDs, with an applied mask of how much the elevator is raised.
-   *
-   * @param percent A double supplier that supplies the elevator's percent raised.
-   */
-  public Command elevatorLED(DoubleSupplier percent) {
-    return set(
-        LEDPattern.gradient(LEDPattern.GradientType.kDiscontinuous, Color.kGreen, Color.kYellow)
-            .mask(LEDPattern.progressMaskLayer(percent)));
-  }
-
-  /** A gradient of green to yellow LEDs, moving at 60 bpm, which synchronizes with many song. */
-  public Command music() {
-    return set(
-        LEDPattern.gradient(LEDPattern.GradientType.kDiscontinuous, Color.kGreen, Color.kYellow)
-            .mask(
-                LEDPattern.progressMaskLayer(
-                    () ->
-                        Math.sin(RobotController.getMeasureTime().in(Seconds) * Math.PI * 2) / 3
-                            + 0.5)));
-  }
-
-  /** An alernating pattern of yellow and green that is scrolled through. */
-  public Command scrolling() {
-    return set(
-        alternatingColor(Color.kYellow, 6, Color.kGreen, 10)
-            .scrollAtAbsoluteSpeed(MetersPerSecond.of(0.5), LED_SPACING));
-  }
-
-  /** A breathing gradient that matches the alliance colors. */
-  public Command autos() {
-    if (Constants.alliance() == Alliance.Red) {
-      return set(
-          LEDPattern.gradient(
-                  LEDPattern.GradientType.kDiscontinuous, Color.kFirstRed, Color.kOrangeRed)
-              .breathe(Seconds.of(2)));
-    } else {
-      return set(
-          LEDPattern.gradient(LEDPattern.GradientType.kDiscontinuous, Color.kFirstBlue, Color.kAqua)
-              .breathe(Seconds.of(2)));
-    }
-  }
-
   public Command set(LEDPattern pattern) {
     return run(
         () -> {
           pattern.applyTo(buffer);
           led.setData(buffer);
         });
-  }
-
-  private static LEDPattern alternatingColor(
-      Color color1, int color1length, Color color2, int color2length) {
-    return (reader, writer) -> {
-      int bufLen = reader.getLength();
-      for (int i = 0; i < bufLen; i++) {
-        writer.setLED(i, (((i % (color1length + color2length)) < color1length) ? color1 : color2));
-      }
-    };
   }
 
   @Override
