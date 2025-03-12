203ad21a56ef352ab09899bb38e1c7d114dde4f5
metacoderminecraft <metacoderminecraft@gmail.com>
2025-03-11 20:01:29 -0400

    I would like it if just one time I remembered spotless + build

--- src/main/java/org/sciborgs1155/lib/Hydromands.java
+++ src/main/java/org/sciborgs1155/lib/Hydromands.java
@@ -1,24 +1,23 @@
 package org.sciborgs1155.lib;
 
-import java.util.Arrays;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import java.util.Arrays;
 
 public class Hydromands {
-    public static Command sequence(Command... commands) {
-        return Commands.sequence(proxyAll(commands));
-    }
-    
-    public static Command parallel(Command... commands) {
-        return Commands.parallel(proxyAll(commands));
-    }
+  public static Command sequence(Command... commands) {
+    return Commands.sequence(proxyAll(commands));
+  }
 
-    public static Command deadline(Command deadline, Command... otherCommands) {
-        return Commands.deadline(deadline.asProxy(), proxyAll(otherCommands));
-    }
+  public static Command parallel(Command... commands) {
+    return Commands.parallel(proxyAll(commands));
+  }
 
-    public static Command[] proxyAll(Command... commands) {
-        return Arrays.stream(commands).map(command -> command.asProxy()).toArray(Command[]::new);
-    }
+  public static Command deadline(Command deadline, Command... otherCommands) {
+    return Commands.deadline(deadline.asProxy(), proxyAll(otherCommands));
+  }
+
+  public static Command[] proxyAll(Command... commands) {
+    return Arrays.stream(commands).map(command -> command.asProxy()).toArray(Command[]::new);
+  }
 }
