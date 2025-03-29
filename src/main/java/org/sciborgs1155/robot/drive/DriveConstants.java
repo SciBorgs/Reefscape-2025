3db94cc5b030bf94e9cc63dec2a3cbd366b77ccc
sigalrmp <sigalrmp@gmail.com>
2025-03-29 08:59:40 -0400

    cleaning

--- src/main/java/org/sciborgs1155/robot/drive/DriveConstants.java
+++ src/main/java/org/sciborgs1155/robot/drive/DriveConstants.java
@@ -1,14 +1,9 @@
 package org.sciborgs1155.robot.drive;
 
 import static edu.wpi.first.units.Units.*;
-import static org.sciborgs1155.robot.Constants.Robot.MASS;
-import static org.sciborgs1155.robot.Constants.Robot.MOI;
 
-import com.pathplanner.lib.config.ModuleConfig;
-import com.pathplanner.lib.config.RobotConfig;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularAcceleration;
 import edu.wpi.first.units.measure.AngularVelocity;
@@ -78,20 +73,6 @@ public final class DriveConstants {
     new Translation2d(WHEEL_BASE.div(-2), TRACK_WIDTH.div(2)), // rear left
     new Translation2d(WHEEL_BASE.div(-2), TRACK_WIDTH.div(-2)) // rear right
   };
-
-  public static final RobotConfig ROBOT_CONFIG =
-      new RobotConfig(
-          MASS,
-          MOI,
-          new ModuleConfig(
-              WHEEL_RADIUS,
-              MAX_SPEED,
-              WHEEL_COF,
-              DCMotor.getKrakenX60(1),
-              1 / ModuleConstants.Driving.GEARING,
-              ModuleConstants.Driving.STATOR_LIMIT,
-              1),
-          MODULE_OFFSET);
 
   // public static final PathConstraints PATH_CONSTRAINTS =
   //     new PathConstraints(MAX_SPEED, MAX_ACCEL, MAX_ANGULAR_SPEED, MAX_ANGULAR_ACCEL);
