c550a47693f69acff34d33ab751f8dcb91d57eee
sigalrmp <sigalrmp@gmail.com>
2025-04-02 22:44:49 -0400

    zeeroooos

--- src/main/java/org/sciborgs1155/robot/drive/Drive.java
+++ src/main/java/org/sciborgs1155/robot/drive/Drive.java
@@ -152,8 +152,8 @@ public class Drive extends SubsystemBase implements AutoCloseable {
           translationP.get(),
           translationI.get(),
           translationD.get(),
-          new TrapezoidProfile.Constraints(
-              0.04 * MAX_SPEED.in(MetersPerSecond), 0.04 * MAX_ACCEL.in(MetersPerSecondPerSecond)));
+          new TrapezoidProfile.Constraints(0, 0));
+              // 0.04 * MAX_SPEED.in(MetersPerSecond), 0.04 * MAX_ACCEL.in(MetersPerSecondPerSecond)));
 
   @Logged
   private final PIDController rotationController =
